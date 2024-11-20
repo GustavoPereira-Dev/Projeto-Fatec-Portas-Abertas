2c3fcf71d403fa89e1da9e5195b31cf1c94be54a
JM <oshirohiro00@gmail.com>
2024-11-20 10:00:54 -0300

    Update JogoDaVelhaController.java

--- src/projeto/controller/JogoDaVelhaController.java
+++ src/projeto/controller/JogoDaVelhaController.java
@@ -15,69 +15,84 @@ public class JogoDaVelhaController {
     }
 
     public void iniciarJogo() {
+        int linha, coluna;
+        
         while (true) {
-            //Exibir o tabuleiro
-            System.out.println("Tabuleiro atual:");
-            exibirTabuleiro();
-
-            //Inicio da jogada, será digitado linha, depois coluna
-            System.out.println("Digite o número da linha (1, 2 ou 3): ");
-            int linha = obterEntrada() - 1;  
-            System.out.println("Digite o número da coluna (1, 2 ou 3): ");
-            int coluna = obterEntrada() - 1;  
-
             // Fazer a jogada
-            if (estrutura.fazerJogada(linha, coluna)) {
-                if (estrutura.verificarVencedor()) {
-                    exibirTabuleiro();
-                    System.out.println("Jogador " + estrutura.jogadorAtual + " venceu!");
-                    // Opção para reiniciar o jogo:
-                    // System.out.println("Deseja jogar novamente? (S/N)");
-                    // String resposta = scanner.next().toUpperCase();
-                    // if (!resposta.equals("S")) {
-                    //     System.out.println("Obrigado por jogar!");
-                    //     break;
-                    // }
-                    break;
-                } else if (estrutura.verificarEmpate()) {
-                    exibirTabuleiro();
-                    System.out.println("Empate!");
-                    // Opção para reiniciar o jogo.
-                    // System.out.println("Deseja jogar novamente? (S/N)");
-                    // String resposta = scanner.next().toUpperCase();
-                    // if (!resposta.equals("S")) {
-                    //     System.out.println("Obrigado por jogar!");
-                    //     break;
-                    // }
-                    break;
+            while (true){ //seria melhor criar um método novo no lugar pra validar??
+                //Exibir o tabuleiro
+                exibirTabuleiro();
+                //Inicio da jogada, será digitado linha, depois coluna
+                linha = obterEntrada("Vez do jogador '" + estrutura.jogadorAtual + "'\nDigite o número da linha (1, 2 ou 3): ") - 1;  
+                coluna = obterEntrada("Digite o número da coluna (1, 2 ou 3): ") - 1;
+
+                if (estrutura.fazerJogada(linha, coluna)){
+                    break; // Valida a jogada
                 }
+                System.out.println("\nJogada inválida, tente novamente");
+            }
+            
+            if (estrutura.verificarVencedor()) {
+                exibirTabuleiro();
+                System.out.println("Jogador " + estrutura.jogadorAtual + " venceu!");
+                // Opção para reiniciar o jogo:
+                // System.out.println("Deseja jogar novamente? (S/N)");
+                // String resposta = scanner.next().toUpperCase();
+                // if (!resposta.equals("S")) {
+                //     System.out.println("Obrigado por jogar!");
+                //     break;
+                // }
+                break;
+            } else if (estrutura.verificarEmpate()) {
+                exibirTabuleiro();
+                System.out.println("Empate!");
+                // Opção para reiniciar o jogo.
+                // System.out.println("Deseja jogar novamente? (S/N)");
+                // String resposta = scanner.next().toUpperCase();
+                // if (!resposta.equals("S")) {
+                //     System.out.println("Obrigado por jogar!");
+                //     break;
+                // }
+                break;
+            }
                 // Trocar o jogador
                 estrutura.trocarJogador();
-            } else {
-                System.out.println("Jogada inválida. Tente novamente.");
-            }
         }
     }
 
     private void exibirTabuleiro() {
         char[][] tabuleiro = estrutura.tabuleiro;
-        System.out.println("  1   2   3");
+        System.out.println("Tabuleiro atual:\n   1   2   3");
         for (int i = 0; i < 3; i++) {
         	// Exibir o número da coluna
             System.out.print((i + 1) + " ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(tabuleiro[i][j] + " "); 
+                System.out.print(" " + tabuleiro[i][j]); 
                 //Criar as divisões das colunas
-                if (j < 2) System.out.print("| ");  
+                if (j < 2) System.out.print(" |");  
             }
             System.out.println();
             if (i < 2) {
-                System.out.println("--------------"); 
+                System.out.println("  ---+---+---"); 
             }
         }
     }
 
-    private int obterEntrada() {
-        return scanner.nextInt();
+    private int obterEntrada(String mensagem) {
+        int entrada = 0;
+        while (true) {
+        	try {
+    	        System.out.println(mensagem);
+    		    entrada = Integer.parseInt(scanner.nextLine());
+    		    if (entrada >= 1 && entrada <= 3) {
+    			    break;
+    		    } else {
+    			    System.out.println("Entrada inválida, tente novamente.");
+    		    }
+    	    } catch (Exception e) {
+    		    System.out.println("Entrada inválida, tente novamente.");
+    	    }
+        }
+        return entrada;
     }
 }
